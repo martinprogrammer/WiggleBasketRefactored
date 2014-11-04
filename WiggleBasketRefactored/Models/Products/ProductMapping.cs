d741901b393841ed4b954664b2a476007ad7c09f
martinprogrammer <martinprogrammer@outlook.com>
2014-11-04 23:34:45 +0000

    Added WebDbContext

---
+++ WiggleBasketRefactored/Models/Products/ProductMapping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace WiggleBasketRefactored.Models.Products
+{
+    public class ProductMapping : EntityTypeConfiguration<Product>
+    {
+        public ProductMapping()
+        {
+            this.HasKey(x => x.ID);
+            this.ToTable("Products");
+        }
+    }
+}
---
+++ WiggleBasketRefactored/Models/Products/WebDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WiggleBasketRefactored.Models.Products
+{
+    public interface IWebDbContext : IDisposable
+    {
+    }
+
+    public class WebDbContext : DBContext, IWebDbContext
+    {
+      
+
+        public WebDbContext()
+        {
+            Configuration.LazyLoadingEnabled = true;
+        }
+
+        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProductMapping());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+    
+}
